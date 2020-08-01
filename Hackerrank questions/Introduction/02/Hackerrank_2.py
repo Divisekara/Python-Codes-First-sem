00d63a407329b38e4b80156358363cdd7eff85ee
Divisekara <9asitha7@gmail.com>
2020-08-01 14:10:09 +0530

    2

---
+++ Hackerrank questions/Introduction/02/Hackerrank_2.py
@@ -0,0 +1,16 @@
+"""
+hackerank 2
+"""
+
+while True:
+    
+    n=int(raw_input('Give a number'))
+    if n%2!=0:
+        print 'Weird'
+    elif 2<=n<5:
+        print 'Not Weird'
+    elif 6<=n<=20:
+        print 'Weird'
+    else
+        print 'Not weird'
+            
---
+++ Hackerrank questions/Introduction/03/HackerRank 3.py
@@ -0,0 +1,8 @@
+"HackerRank 3"
+
+a=int(raw_input())
+b=int(raw_input())
+
+print a+b
+print a-b
+print a*b
